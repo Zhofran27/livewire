fb838e5b7433ada79ac76d8012c2c3e043235bde
Zhofran27 <zhofran275@gmail.com>
2023-09-04 10:26:08 +0700

    create/post

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use app\Livewire\Posts\index;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+//  Route::get('/', function () {
+//      return view('welcome');
+//  });
+
+//posts index
+Route::get('/', App\Livewire\Posts\index::class)->name('posts.index');
+
+//posts create
+Route::get('/create', App\Livewire\Posts\Create::class)->name('posts.create');
+
